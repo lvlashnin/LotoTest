7b607545e3c38c37c16bcb45436aff3623d13426
lvlashnin <lvlashnin@gmail.com>
2025-11-20 22:13:24 +0200

    Configure deployment to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: "/LotoTest/", // Назва вашого репозиторію на GitHub
-  build: { outDir: "dist" },
+  base: "/LotoTest/",
 });
